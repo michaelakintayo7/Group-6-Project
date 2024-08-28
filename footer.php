8e0636fa4b2b9caa8db99ef0629e3b67e0bb7334
michaelakintayo7 <michaelakintayo7@gmail.com>
2024-08-28 23:16:21 +0930

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            flex-direction: column;
+        }
+        
+        .footer {
+            background-color: rgba(0, 123, 255, 0.98); 
+            color: #fff; 
+            padding: 20px;
+            text-align: center;
+            font-size: 14px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1) !important;
+            position: relative;
+            bottom: 0;
+            width: 100%;
+        }
+        .footer p {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <div class="footer">
+        <p>&copy; 2024 ChemManager. All rights reserved.</p>
+    </div>
+</body>
+</html>
