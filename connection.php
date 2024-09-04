ad435f726ea1527cecfe182a418a7ae01773cdc1
mo-guai <for.mo.guai@gmail.com>
2024-09-05 09:22:25 +0930

    no display connecting status on web

--- connection.php
+++ connection.php
@@ -7,8 +7,8 @@ $db = "derikdme_chemManager_db";
 
 $conn = new mysqli($server, $username, $password, $db);
 // Check connection
-if ($conn) {
-    echo "<div class='message'><p>Connected to the database successfully.</p></div><br>";
-} else {
-    echo "<div class='message'><p>Failed to connect to the database: " . mysqli_connect_error() . "</p></div><br>";
-}
+// if ($conn) {
+//     echo "<div class='message'><p>Connected to the database successfully.</p></div><br>";
+// } else {
+//     echo "<div class='message'><p>Failed to connect to the database: " . mysqli_connect_error() . "</p></div><br>";
+// }
