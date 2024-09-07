983ee205487e7e0b0f407e379af9ee1f42e40c41
michaelakintayo7 <michaelakintayo7@gmail.com>
2024-09-07 11:18:17 +0930

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = "server133.web-hosting.com";
+$username = "derikdme_morgen";
+$password = "ShMoJaKa123";
+$db = "derikdme_chemManager_db";
+
+$conn = new mysqli($server, $username, $password, $db);
+// Check connection
+// if ($conn) {
+//     echo "<div class='message'><p>Connected to the database successfully.</p></div><br>";
+// } else {
+//     echo "<div class='message'><p>Failed to connect to the database: " . mysqli_connect_error() . "</p></div><br>";
+// }
